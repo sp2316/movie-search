f9e6bfd83a5dadf6cdfed1a0a5b4113384396369
Sai prasad <sp@MacBook-Pro.local>
2021-04-22 21:26:03 +0530

    Create action types and action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,10 @@
-{
-    type:'ADD_MOVIES',
-    movies:[]
+//action types,used as variables instead of directly assigning as string to type
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+//action creators
+export function addMovies(movies){
+    return{
+        type:ADD_MOVIES,     //{---} Action
+        movies:movies
+    }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
+import {ADD_MOVIES} from '../actions';
+
 export default function movies(state=[],action){  //current state of store
     
-    if(action.type === 'ADD_MOVIES'){
+    if(action.type === ADD_MOVIES){
         return action.movies; //return new state..dont append it to the original state
     }
     return state; //if no defined action,return the unmodified original state
