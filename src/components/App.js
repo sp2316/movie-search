89e1b152957bf67383e6359030b05a163b965d75
Sai prasad <sp@MacBook-Pro.local>
2021-04-26 12:29:54 +0530

    Change the state(make it an object instead of an array)

--- src/components/App.js
+++ src/components/App.js
@@ -20,9 +20,9 @@ class App extends React.Component {
   }
 
   render(){
-    const movies=this.props.store.getState();
+    const { list }=this.props.store.getState(); //{list:[],favourites:[]}
 
-    console.log('RENDER')
+    console.log('RENDER',this.props.store.getState());
 
     return (
         <div className="App">
@@ -35,7 +35,7 @@ class App extends React.Component {
             </div>
 
             <div className="list">
-              {movies.map((movie,index) => (
+              {list.map((movie,index) => (
                 <MovieCard movie={movie} key={`movies-${index}`}/>
               ))}
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
 import {ADD_MOVIES} from '../actions';
 
-export default function movies(state=[],action){  //current state of store
+const initialMoviesState={
+    list:[],
+    favourites:[]
+}
+
+export default function movies(state=initialMoviesState,action){  //current state of store
     
     if(action.type === ADD_MOVIES){
-        return action.movies; //return new state..dont append it to the original state
+        return {
+            ...state,
+            list:action.movies
+        } //return new state..dont append it to the original state
     }
     return state; //if no defined action,return the unmodified original state
 
