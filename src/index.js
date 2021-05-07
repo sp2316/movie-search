4fe4b5e1ac5da8639bf798ce9db545e55c45d06f
Sai prasad <sp@MacBook-Pro.local>
2021-05-08 00:32:43 +0530

    Add middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 
 import './index.css';
 import App from './components/App'
 import rootReducer from './reducers'
+// import haha from './reducers' check by using thus in createStore()
 
-const store=createStore(rootReducer);
+//curried form of function logger(obj,next,action)
+//  [[ [logger(obj)] (next)](action)]
+const logger = function({dispatch,getState}){
+    return function(next){
+      return function(action){
+        //middleware code
+        console.log('ACTION_TYPE',action.type)
+        next(action);
+      }
+    }
+
+}
+
+
+const store=createStore(rootReducer,applyMiddleware(logger)); //no matter what reducer you call, the combineReducers() is only called here
+
+
+
 
 //console.log('store',store);
 //console.log('BEFORE STATE',store.getState());
