16cbc87e4f7897b1923fefb370b109a4d33137a0
Sai prasad <sp@MacBook-Pro.local>
2021-05-07 16:10:50 +0530

    Add root reducer and search reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
 import {ADD_MOVIES,ADD_TO_FAVOURITES,REMOVE_FROM_FAVOURITES,SET_SHOW_FAVOURITES} from '../actions';
 
+//state will be undefined in the beginning,so we use default parameters
 const initialMoviesState={
     list:[],
     favourites:[],
     showFavourites:false
 }
 
-export default function movies(state=initialMoviesState,action){  //current state of store
+export function movies(state=initialMoviesState,action){  //current state of store
     
      //return new state..dont append it to the original state
     //if no defined action,return the unmodified original state
@@ -39,4 +40,31 @@ export default function movies(state=initialMoviesState,action){  //current stat
                 return state;
 
     }
+}
+
+const initialSearchState={
+    result:{}
+}
+
+export function search(state=initialSearchState,action){
+
+return state;
+    
+
+
+}
+
+const initialRootState={
+    movies:initialMoviesState,
+    search:initialSearchState
+}
+
+export default function rootReducer(state=initialRootState,action){
+
+ return{
+     movies:movies(state.movies,action),
+     seatch:search(state.search,action)
+ }
+
+
 }
